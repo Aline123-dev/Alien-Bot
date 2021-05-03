be33bc0929cd2fabedd0f3b8c3c283d9b4610f32
Aline123-dev <79563723+Aline123-dev@users.noreply.github.com>
2021-05-03 10:00:51 +0500

    Hi everyone this is Alien Bot
    Engine : pyttsx3

---
+++ AlienBot.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hi thos is alien a computerized robot based on ALIE11 alien O S my owner is talha i am your friend and your assistant soon iam in work sorry you can not now use me please use variable and write my code in Python ")
+engine.runAndWait()
